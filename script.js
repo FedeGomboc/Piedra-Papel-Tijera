028a72d1445aac4ccf552d82a4f0bf58762d373e
FedeGomboc <47128235@est.ort.edu.ar>
2023-12-18 15:05:24 -0300

    agregue front

--- script.js
+++ script.js
@@ -1,107 +1,81 @@
-var contadorPlayer = 0;
-var contadorComputer = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+botones = document.querySelectorAll("button");
+restartButton = document.querySelector(".playAgain");
+
+botones.forEach((button) => {
+  button.addEventListener("click", function () {
+    playRound(button.value);
+  });
+});
+
+restartButton.addEventListener("click", function () {
+  restartGame();
+});
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3) + 1;
 
   switch (choice) {
     case 1:
-      console.log("Computer: Rock");
-      return "Rock";
-      break;
+      return "rock";
     case 2:
-      console.log("Computer: Paper");
-      return "Paper";
-      break;
+      return "paper";
     case 3:
-      console.log("Computer: Scissors");
-      return "Scissors";
-      break;
-    default:
-      return null;
+      return "scissors";
   }
 }
 
-function playRound(playerSelection, computerSelection) {
-  switch (playerSelection) {
-    case "rock":
-      switch (computerSelection) {
-        case "Rock":
-          return "It's a tie";
-          break;
-        case "Paper":
-          contadorComputer = contadorComputer + 1;
-          return "Computer wins, Paper beats Rock";
-          break;
-        case "Scissors":
-          contadorPlayer = contadorPlayer + 1;
-          return "Player wins, Rock beats Scissors";
-          break;
-      }
-      break;
-    case "paper":
-      switch (computerSelection) {
-        case "Rock":
-          contadorPlayer = contadorPlayer + 1;
-          return "Player wins, Paper beats Rock";
-          break;
-        case "Paper":
-          return "It's a tie";
-          break;
-        case "Scissors":
-          contadorComputer = contadorComputer + 1;
-          return "Computer wins, Scissors beats Paper";
-          break;
-      }
-      break;
-    case "scissors":
-      switch (computerSelection) {
-        case "Rock":
-          contadorComputer = contadorComputer + 1;
-          return "Computer wins, Rock beats Scissors";
-          break;
-        case "Paper":
-          contadorPlayer = contadorPlayer + 1;
-          return "Player wins, Scissors beats Paper";
-          break;
-        case "Scissors":
-          return "It's a tie";
-          break;
-      }
-      break;
-    default:
-      return null;
-  }
-}
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+  let result = "";
+  resultScore = "";
 
-function game() {
-  while (contadorPlayer < 5 && contadorComputer < 5) {
-    const playerSelection = prompt("Enter your selection").toLowerCase();
-    const computerSelection = getComputerChoice();
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+  ) {
+    playerScore = playerScore + 1;
+    result = "You win! " + playerSelection + " beats " + computerSelection;
 
-    if (playerSelection !== null) {
-      round = playRound(playerSelection, computerSelection);
+    if (playerScore === 5) {
+      result = "Player won the game!";
+      disableButtons();
+    }
+  } else if (playerSelection == computerSelection) {
+    result = "It's a tie. You both chose " + playerSelection;
+  } else {
+    computerScore = computerScore + 1;
+    result = "You lose! " + computerSelection + " beats " + playerSelection;
 
-      while (round === "It's a tie") {
-        console.log(round);
-        const playerSelection = prompt(
-          "It was a tie, enter your selection again"
-        ).toLowerCase();
-        const computerSelection = getComputerChoice();
-        round = playRound(playerSelection, computerSelection);
-      }
-
-      console.log(round);
-      console.log("Player: " + contadorPlayer)
-      console.log("Computer: " + contadorComputer)
+    if (computerScore === 5) {
+      result = "Computer won the game!";
+      disableButtons();
     }
   }
 
-  if (contadorComputer === 5) {
-    console.log("Computer wins");
-  } else if (contadorPlayer === 5) {
-    console.log("Player wins");
-  }
+  document.getElementById("result").innerHTML = result;
+  document.querySelector(".playerScore").innerHTML = playerScore;
+  document.querySelector(".computerScore").innerHTML = computerScore;
+  return;
 }
 
-game()
+function disableButtons() {
+  botones.forEach((elem) => {
+    elem.disabled = true;
+  });
+}
+
+restartGame = () => {
+  playerScore = 0;
+  computerScore = 0;
+  botones.forEach((elem) => {
+    elem.disabled = false;
+  });
+  result = "";
+  document.getElementById("result").innerHTML = result;
+  document.querySelector(".playerScore").innerHTML = playerScore;
+  document.querySelector(".computerScore").innerHTML = computerScore;
+};
